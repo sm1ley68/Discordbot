1ba158123188e567ab23b5e35c230103afc5131a
Buddha <fionovitch@yandex.ru>
2023-12-15 13:40:11 +0300

    попытался в классы

--- Discordbot.py
+++ Discordbot.py
@@ -6,6 +6,8 @@ from disnake.ext import commands
 import random
 import os
 
+from Moderation import Moderation
+
 bot = commands.Bot(command_prefix="$", help_command=None, intents=disnake.Intents.all(),
                    test_guilds=[793728158150557707])
 
@@ -16,6 +18,7 @@ CENSORED_WORDS = []
 async def on_ready():
     print(f'Bot {bot.user} in ready to work!')
 
+moderation = Moderation()
 
 # @bot.event
 # async def panel_menu(member):
@@ -53,43 +56,19 @@ async def on_message(message):
 
 
 # кик пользователя
-@bot.command(name='кик', help='Выгнать пользователя(Только администрация)')
-@commands.has_permissions(kick_members=True, administrator=True)
-async def kick(ctx, member: disnake.Member, *, reason='Нарушение правил.'):
-    await ctx.send(
-        f'Администратор {ctx.author.mention} исключил пользователя {member.mention} "Нарушение правил сервера"')
-    await member.kick(reason=reason)
-    await ctx.message.delete()
+moderation.kick()
 
 
 # мут пользователя
-@bot.command(name='мут', help='Замьютит участника(Только администрация)')
-@commands.has_permissions(administrator=True)
-async def mute(ctx, member: disnake.Member, *, reason=None):
-    mute_role = disnake.utils.get(ctx.message.guild.roles, name='петушок молчит')
-
-    await member.add_roles(mute_role)
-    await ctx.send(f'{ctx.author.mention} замутил {member.mention} по причине {reason}.')
+moderation.mute()
 
 
 # анмут пользователя
-@bot.command(name='анмут', help='Размьютить участника(Только администрация)')
-@commands.has_permissions(administrator=True)
-async def unmute(ctx, member: disnake.Member):
-    mute_role = disnake.utils.get(ctx.guild.roles, name='петушок молчит')
-
-    await member.remove_roles(mute_role)
-    await ctx.send(f'{ctx.author.mention} размутил {member.mention}.')
+moderation.anmute()
 
 
 # бан пользователя
-@bot.command(name='бан', help='Забанить пользователя(Только администрация)')
-@commands.has_permissions(ban_members=True, administrator=True)
-async def ban(ctx, member: disnake.Member, *, reason='Нарушение правил.'):
-    await ctx.send(
-        f'Администратор {ctx.author.mention} забанил пользователя {member.mention} "Нарушение правил сервера"')
-    await member.ban(reason=reason)
-    await ctx.message.delete()
+moderation.ban()
 
 
 # бросок кубика
